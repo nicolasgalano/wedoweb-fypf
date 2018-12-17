6811991aba5f369e77bffa17d2d2dd6af8372c7a
Nicolas Galano <nicolas.galano@gmail.com>
2018-12-16 21:59:28 -0300

    Home End

--- src/js/z-home.js
+++ src/js/z-home.js
@@ -18,8 +18,8 @@
         var swiper = new Swiper('.swiper-container', {
             loop: true,
             autoplay: {
-                delay: 3000,
-                disableOnInteraction: false,
+                delay: 4500,
+                disableOnInteraction: true,
             },
             navigation: {
                 nextEl: '.swiper-button-next',
@@ -27,9 +27,43 @@
             }
         });
 
+        //VIDEOS
         $('.thumbnail').click(function(){
-            console.log('https://www.youtube.com/embed/'+$(this).attr('data-yt'));
             $('.videos .primario iframe').attr('src','https://www.youtube.com/embed/'+$(this).data('yt'));
+        });
+
+        //ALIANZAS
+        $('.row-alianzas ul.botones li').click(function(){
+            $('.row-alianzas .alianzas-tabs .tab').removeClass('active');
+            $('.row-alianzas .alianzas-tabs .tab.tab-'+$(this).data('tab')).addClass('active');
+        });
+        $('.row-alianzas .selectioner select').change(function() {
+            $('.row-alianzas .alianzas-tabs .tab').removeClass('active');
+            $('.row-alianzas .alianzas-tabs .tab.'+$(this).val()).addClass('active');
+        });
+
+        //MENU
+        $('nav a.btn-menu').click(function(){
+            $('nav ul.menu').toggleClass('active');
+        });
+
+        //DAMERO
+        $('.row-damero ul.categorias li').click(function(){
+            $('.row-damero .links a').addClass('hidden');
+            var array = $(this).data('array');
+            for(i=0 ; i<array.length ; i++){
+                $('.row-damero .links a#link-'+array[i]).removeClass('hidden');
+            }
+        });
+        $('.row-damero .selectioner select').change(function() {
+            $('.row-damero .links a').addClass('hidden');
+            var array = $(this).val();
+            array = array.replace('[', '');
+            array = array.replace(']', '');
+            array = array.split(',');
+            for(i=0 ; i<array.length ; i++){
+                $('.row-damero .links a#link-'+array[i]).removeClass('hidden');
+            }
         });
 
     });
