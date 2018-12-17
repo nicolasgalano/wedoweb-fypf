622e3b221a5ad1ca26142a2b8683dd92b7f5b9c1
Nicolas Galano <nicolas.galano@gmail.com>
2018-12-17 16:43:32 -0300

    Latest

--- src/js/z-home.js
+++ src/js/z-home.js
@@ -2,6 +2,8 @@
 
     $(document).ready(function(){
 
+        $('html').smoothScroll(1000);
+
         //WOW
         wow = new WOW(
             {
@@ -15,15 +17,29 @@
         wow.init();
 
         //SWIPER
-        var swiper = new Swiper('.swiper-container', {
+        var swiper = new Swiper('.row-premios .swiper-container', {
             loop: true,
             autoplay: {
                 delay: 4500,
                 disableOnInteraction: true,
             },
+            preventClicks:false,
+            preventClicksPropagation:false,
             navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
+                nextEl: '.row-premios .swiper-button-next',
+                prevEl: '.row-premios .swiper-button-prev',
+            }
+        });
+
+        //SWIPER 2
+        var swiper2 = new Swiper('.videos .swiper-container', {
+            slidesPerView: 6,
+            spaceBetween: 0,
+            preventClicks:false,
+            preventClicksPropagation:false,
+            navigation: {
+                nextEl: '.videos .swiper-button-next',
+                prevEl: '.videos .swiper-button-prev',
             }
         });
 
@@ -33,7 +49,9 @@
         });
 
         //ALIANZAS
-        $('.row-alianzas ul.botones li').click(function(){
+        $('.row-alianzas ul.botones li').click(function() {
+            $('.row-alianzas ul.botones li').removeClass('active');
+            $(this).addClass('active');
             $('.row-alianzas .alianzas-tabs .tab').removeClass('active');
             $('.row-alianzas .alianzas-tabs .tab.tab-'+$(this).data('tab')).addClass('active');
         });
@@ -44,25 +62,43 @@
 
         //MENU
         $('nav a.btn-menu').click(function(){
-            $('nav ul.menu').toggleClass('active');
+            $('nav').toggleClass('mobile-open');
+        });
+        $('nav.mobile-open ul.menu a').click(function(){
+            $('nav').removeClass('mobile-open');
         });
 
         //DAMERO
+        $('.row-damero .links > div').click(function(){
+            $(this).toggleClass('clicked');
+        });
+
         $('.row-damero ul.categorias li').click(function(){
-            $('.row-damero .links a').addClass('hidden');
+            $('.row-damero ul.categorias li').removeClass('active');
+            $('.row-damero .links > div').removeClass('hidden');
+            $(this).addClass('active');
+            $('.row-damero .links > div').removeClass('seleccionado');
             var array = $(this).data('array');
             for(i=0 ; i<array.length ; i++){
-                $('.row-damero .links a#link-'+array[i]).removeClass('hidden');
+                $('.row-damero .links div#link-'+array[i]).addClass('seleccionado');
             }
         });
         $('.row-damero .selectioner select').change(function() {
-            $('.row-damero .links a').addClass('hidden');
+            $('.row-damero .links > div').removeClass('seleccionado');
+            $('.row-damero .links > div').addClass('hidden');
             var array = $(this).val();
             array = array.replace('[', '');
             array = array.replace(']', '');
             array = array.split(',');
             for(i=0 ; i<array.length ; i++){
-                $('.row-damero .links a#link-'+array[i]).removeClass('hidden');
+                if(array[i] == 0){
+                    console.log('asdasdasdasd');
+                    $('.row-damero .links > div').removeClass('seleccionado');
+                    $('.row-damero .links > div').removeClass('hidden');
+                }else{
+                    $('.row-damero .links div#link-'+array[i]).addClass('seleccionado');
+                    $('.row-damero .links div#link-'+array[i]).removeClass('hidden');
+                }
             }
         });
 
